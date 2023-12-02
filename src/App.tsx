b6bf305ac613268a4abca95fdc350d9e018d962a
RamiroCyber <ramiro.dr@gmail.com>
2023-12-02 14:52:51 -0300

    criando componente uploader

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { } from 'react';
 import './App.css'
+import Uploader from "./Uploader.tsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+const FileUploadComponent: React.FC = () => {
+    // const [message, setMessage] = useState<string>('');
+    //
+    // const handleMessageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //     setMessage(event.target.value);
+    // };
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    return (
+        <div>
+            <Uploader></Uploader>
+            <button type="submit" style={{
+                width: '100%',
+                padding: '10px',
+                backgroundColor: '#28a745',
+                color: 'white',
+                border: 'none',
+                borderRadius: '5px',
+                cursor: 'pointer'
+            }}>Enviar
+            </button>
+        </div>
 
-export default App
+    );
+};
+
+export default FileUploadComponent;
